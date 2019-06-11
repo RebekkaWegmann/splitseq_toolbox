952d3dda3fbfbf06b4383c426a066a1258a67b04
Rebekka Wegmann <wegmann@imsb.biol.ethz.ch>
2019-06-11 08:57:57 +0200

    collect_UMI_read_relation now uses a relatve path to check_UMIs_2.py

--- src/misc/collect_UMI_read_relation.sh
+++ src/misc/collect_UMI_read_relation.sh
@@ -3,8 +3,9 @@
 # This is to run the check_UMIs script over a list of CBCs
 # The list has to be provided as a text file with one CBC per line
 
+DIRECTORY=`dirname $0`
 while read fraction;
 do
 	echo $fraction
-	java -jar $PICARD DownsampleSam I=gene_function_tagged.bam P=$fraction M=downsample_metrics.txt O=/dev/stdout | python $HOME/nas_1/Data/Rebekka/Splitseq_mapping/check_UMIs_2.py "-"
+	java -jar $PICARD DownsampleSam I=gene_function_tagged.bam P=$fraction M=downsample_metrics.txt O=/dev/stdout | python $DIRECTORY/check_UMIs_2.py "-"
 done < fractions.txt
